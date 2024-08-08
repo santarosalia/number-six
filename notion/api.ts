992265b151bc843446af299da50ffeb17d219cf5
gqob <dhkim@edentns.com>
2024-08-08 23:43:48 +0900

    fix: count

--- notion/api.ts
+++ notion/api.ts
@@ -38,21 +38,41 @@ export const read = async () => {
     });
     // console.log(result.results[0].properties.num.multi_select)
 }
-export const getThisWeekCount = async (cursor: string | null, count: number) => {
+export const getThisWeekCount = async (cursor: string | null, count: number): Promise<number> => {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // 0 (Sunday) to 6 (Saturday)
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1)); // Monday
+    startOfWeek.setHours(0);
+    startOfWeek.setMinutes(0);
+    startOfWeek.setSeconds(0);
+    const endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6); // Sunday
+
     const notion = getNotion();
     const result = await notion.databases.query({
         database_id: 'a3b896c6cad44549b5fe1d218d997d87',
         filter: {
-            property: 'created',
-            created_time: {
-                this_week: {}
-            }
+            and: [
+                {
+                    property: 'created',
+                    date: {
+                        on_or_after: startOfWeek.toISOString()
+                    }
+                },
+                {
+                    property: 'created',
+                    date: {
+                        on_or_before: endOfWeek.toISOString()
+                    }
+                }
+            ]
         },
         start_cursor: cursor ?? undefined
     });
     count += result.results.length;
     if (result.has_more) {
-        count += await getThisWeekCount(result.next_cursor, count);
+        return await getThisWeekCount(result.next_cursor, count);
     } 
     return count;
     
