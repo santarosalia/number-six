a884a371291b9bf44fd172335387a9ca337b196c
gqob <dhkim@edentns.com>
2024-08-11 01:39:58 +0900

    feat: time console

--- db/api.ts
+++ db/api.ts
@@ -24,6 +24,9 @@ export const getThisWeekCount = async () => {
     startOfWeek.setMinutes(0);
     startOfWeek.setSeconds(0);
     const endOfWeek = new Date(startOfWeek);
+    console.log(startOfWeek)
+    console.log(endOfWeek)
+    
     endOfWeek.setDate(startOfWeek.getDate() + 6); // Sunday
     const count = await coll.countDocuments({
         created: {
