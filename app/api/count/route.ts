a611aa79775ca964a5ecd7fdb2acbb0e3006e75a
gqob <dhkim@edentns.com>
2024-08-12 23:16:06 +0900

    test: change method

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -1,7 +1,7 @@
 import { getThisWeekCount } from "@/db/api";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest) => {
-    const count = await getThisWeekCount();
-    return new NextResponse(JSON.stringify({count: count}));
-}
+export const POST = async (req: NextRequest) => {
+  const count = await getThisWeekCount();
+  return new NextResponse(JSON.stringify({ count: count }));
+};
--- components/ThisWeek.tsx
+++ components/ThisWeek.tsx
@@ -1,36 +1,39 @@
-'use client'
+"use client";
 import { useMainStore } from "@/store/store";
 import { useEffect } from "react";
 
 const ThisWeek = () => {
-    const { thisWeekCount, setThisWeekCount, sorted } = useMainStore();
+  const { thisWeekCount, setThisWeekCount, sorted } = useMainStore();
 
-    useEffect(() => {
-        const thisWeek = async () => {
-            const res = await fetch('/api/count', {
-                method: 'GET',
-            });
-            const result = await res.json();
-            setThisWeekCount(result.count);
-        }
-        thisWeek();
-    }, []);
-    if (thisWeekCount === 0) {
-        return (
-            <span className="text-sm text-gray-50 flex items-center gap-2">
-                <span>이번 주 생성 횟수:</span>
-                <div style={{
-                        height: '1px',
-                        width: '10px',
-                        border: '1px solid white',
-                        animation: 'spin 1s ease infinite'
-                    }}
-                    ></div>
-            </span>
-        )
-    }
+  useEffect(() => {
+    const thisWeek = async () => {
+      const res = await fetch("/api/count", {
+        method: "POST",
+      });
+      const result = await res.json();
+      setThisWeekCount(result.count);
+    };
+    thisWeek();
+  }, []);
+  if (thisWeekCount === 0) {
     return (
-        <span className="text-sm text-gray-50">이번 주 생성 횟수: {thisWeekCount}</span>
-    )
-}
+      <span className="text-sm text-gray-50 flex items-center gap-2">
+        <span>이번 주 생성 횟수:</span>
+        <div
+          style={{
+            height: "1px",
+            width: "10px",
+            border: "1px solid white",
+            animation: "spin 1s ease infinite",
+          }}
+        ></div>
+      </span>
+    );
+  }
+  return (
+    <span className="text-sm text-gray-50">
+      이번 주 생성 횟수: {thisWeekCount}
+    </span>
+  );
+};
 export default ThisWeek;
