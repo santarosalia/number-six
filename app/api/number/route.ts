3661f11b54db9c142bf0b11f9c9eb1c620233016
gqob <dhkim@edentns.com>
2024-08-12 21:29:38 +0900

    fix: time

--- app/api/number/route.ts
+++ app/api/number/route.ts
@@ -1,14 +1,14 @@
 // import { write } from "@/notion/api";
-import {write} from '@/db/api';
+import { write } from "@/db/api";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    const numbers = await req.json() as number[];
-    const date = new Date(new Date() + 9 * 60 * 60 * 1000);
-    const item = {
-        created: date,
-        numbers: numbers
-    }
-    await write(item);
-    return new NextResponse();
-}
+  const numbers = (await req.json()) as number[];
+  const date = new Date(Date.now() + 9 * 60 * 60 * 1000);
+  const item = {
+    created: date,
+    numbers: numbers,
+  };
+  await write(item);
+  return new NextResponse();
+};
--- db/api.ts
+++ db/api.ts
@@ -1,34 +1,30 @@
 import prisma from "@/lib/prisma";
 
-export const write = async (item: {
-    created: Date,
-    numbers: number[]
-}) => {
-    await prisma.number.create({
-        data: item
-    });
-}
+export const write = async (item: { created: Date; numbers: number[] }) => {
+  await prisma.number.create({
+    data: item,
+  });
+};
 
 export const getThisWeekCount = async () => {
-  
-    const today = new Date(new Date() + 9 * 60 * 60 * 1000);
-    const dayOfWeek = today.getDay(); // 0 (Sunday) to 6 (Saturday)
-    const startOfWeek = new Date(today);
-    startOfWeek.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -5 : 1)); // Monday
-    startOfWeek.setHours(0);
-    startOfWeek.setMinutes(0);
-    startOfWeek.setSeconds(0);
-    const endOfWeek = new Date(startOfWeek);
-    
-    endOfWeek.setDate(startOfWeek.getDate() + 6); // Sunday
-    const count = await prisma.number.count({
-        where: {
-            created: {
-                gte: startOfWeek,
-                lt: endOfWeek
-            }
-        }
-    })
-    
-    return count;
-}
+  const today = new Date(Date.now() + 9 * 60 * 60 * 1000);
+  const dayOfWeek = today.getDay(); // 0 (Sunday) to 6 (Saturday)
+  const startOfWeek = new Date(today);
+  startOfWeek.setDate(today.getDate() - dayOfWeek + (dayOfWeek === 0 ? -5 : 1)); // Monday
+  startOfWeek.setHours(0);
+  startOfWeek.setMinutes(0);
+  startOfWeek.setSeconds(0);
+  const endOfWeek = new Date(startOfWeek);
+
+  endOfWeek.setDate(startOfWeek.getDate() + 6); // Sunday
+  const count = await prisma.number.count({
+    where: {
+      created: {
+        gte: startOfWeek,
+        lt: endOfWeek,
+      },
+    },
+  });
+
+  return count;
+};
