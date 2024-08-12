f40ec685768e6b9f7ecf09bacdcc42574b716fd7
santarosalia <94663227+santarosalia@users.noreply.github.com>
2024-08-12 09:44:49 +0900

    Update route.ts

--- app/api/number/route.ts
+++ app/api/number/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
     const numbers = await req.json() as number[];
-    const date = new Date();
+    const date = new Date(new Date() + 9 * 60 * 60 * 1000);
     const item = {
         created: date,
         numbers: numbers
